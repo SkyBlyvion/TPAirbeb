5766fff692ce66d68c00aae17dd06a698f0a7e4b
Lopez Nicolas <richtofen60@gmail.com>
2024-01-22 11:41:45 +0100

    initial commit

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route("/homer", name: "accueil")]
+    public function home()
+    {
+        // $lien = "<a href='page/2'>liens vers la page 2</a>";
+        // $lien2 = "<a href='page/3'>liens vers la page 3</a>";
+        // $page = $this->html("Hello World <br/>"."$lien<br/>"."$lien2", "page d'accueil");
+        // return new Response($page);
+
+        return $this->render("home/home.html.twig", []);
+    }
+
+    #[Route("/page/{numPage}", name: "page")]
+    public function page(string $numPage)
+    {
+        
+        return new Response($this->html("Bienvenue sur la page", "page", $numPage));
+    }
+
+    public function aboutbook()
+    {
+        return new Response("C'est à propos des livres !!");
+    }
+
+    /**
+     * function de construction de la page
+     * @param $titre
+     * @return String
+     */
+
+    private function html($message, $titre, $number = ""): String
+    {
+        $html ="<html><head><title>$titre</title></head><body><p>$message</p>"
+        ."<p>$number</p></body></html>";
+
+        return $html;
+    }
+}
