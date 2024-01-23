c324f35a646701fda5d76eeaf64359c786e8db83
Lopez Nicolas <richtofen60@gmail.com>
2024-01-23 11:35:09 +0100

    adding annonces view/searchbar and css @media for annonces

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -11,26 +11,24 @@ class HomeController extends AbstractController
     #[Route("/accueil", name: "accueil")]
     public function home()
     {
-        // $lien = "<a href='page/2'>liens vers la page 2</a>";
-        // $lien2 = "<a href='page/3'>liens vers la page 3</a>";
-        // $page = $this->html("Hello World <br/>"."$lien<br/>"."$lien2", "page d'accueil");
-        // return new Response($page);
-
         return $this->render("home/home.html.twig", []);
     }
 
-    #[Route("/page/{numPage}", name: "page")]
-    public function page(string $numPage)
+
+    #[Route("/annonces", name: "annonces")]
+    public function index()
     {
+        return $this->render("annonces/annonce.html.twig", []);
+    }
+
+    // #[Route("/page/{numPage}", name: "page")]
+    // public function page(string $numPage)
+    // {
         
-        return new Response($this->html("Bienvenue sur la page", "page", $numPage));
-    }
+    //     return new Response($this->html("Bienvenue sur la page", "page", $numPage));
+    // }
 
-    public function aboutbook()
-    {
-        return new Response("C'est à propos des livres !!");
-    }
-
+  
     /**
      * function de construction de la page
      * @param $titre
