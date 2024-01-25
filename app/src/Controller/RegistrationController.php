d45d9d6dba1a1acd0a88c1b9b05c5b5016ba8c37
Lopez Nicolas <richtofen60@gmail.com>
2024-01-25 14:28:16 +0100

    modifying REgistration and Auth/repo/methods/controller

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -32,12 +32,12 @@ class RegistrationController extends AbstractController
             // set default role
             $user->setRoles(['ROLE_USER']);
 
-            //persist
+            //persits
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('accueil');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
