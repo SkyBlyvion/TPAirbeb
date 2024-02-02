8a8949eaff14ba699cd011d8de5a4794560c988f
Lopez Nicolas <richtofen60@gmail.com>
2024-02-02 11:54:55 +0100

    gitbugresolved

--- app/src/Entity/Favoris.php
+++ app/src/Entity/Favoris.php
@@ -16,59 +16,27 @@ class Favoris
    #[ORM\Column]
    private ?int $id = null;
 
-<<<<<<< HEAD
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $User = null;
-
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Annonce $Annonce = null;
-=======
 
    #[ORM\ManyToOne(inversedBy: 'annonce')]
    #[ORM\JoinColumn(nullable: false)]
    private ?User $user = null;
->>>>>>> 527113a (resolved favorisTable bug)
 
 
-<<<<<<< HEAD
-    public function getUser(): ?User
-    {
-        return $this->User;
-    }
-
-    public function setUser(?User $User): static
-    {
-        $this->User = $User;
-=======
    #[ORM\ManyToOne(inversedBy: 'favoris')]
    #[ORM\JoinColumn(nullable: false)]
    private ?Annonce $annonce = null;
 
->>>>>>> 527113a (resolved favorisTable bug)
 
    public function getId(): ?int
    {
        return $this->id;
    }
 
-<<<<<<< HEAD
-    public function getAnnonce(): ?Annonce
-    {
-        return $this->Annonce;
-    }
-
-    public function setAnnonce(?Annonce $Annonce): static
-    {
-        $this->Annonce = $Annonce;
-=======
 
    public function getUser(): ?User
    {
        return $this->user;
    }
->>>>>>> 527113a (resolved favorisTable bug)
 
 
    public function setUser(?User $user): static
